399ef1920d4379a9893f855f855482f003e57913
Lital Zubery <lital.z@fiverr.com>
2013-12-05 19:15:22 +0200

    finish 7th exerices

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,11 +13,26 @@ describe "UserPages" do
   	end
 
     describe "signup" do
+      
       before { visit signup_path }
+      
       let(:submit) {"Create my account"}
+      
       describe "with invalid information" do
         it "should not create a user" do
           expect {click_button submit}.not_to change(User,:count)
+        end
+
+        describe "after submission" do
+          before {click_button submit}
+
+          it {should have_title('Sign up')}
+          it {should have_content('error')}
+          it {should have_content('Name can\'t be blank')}
+          it {should have_content('Email can\'t be blank')}
+          it {should have_content('Email is invalid')}
+          it {should have_content('Password can\'t be blank')}
+          it {should have_content('minimum is 6 characters')}
         end
       end
 
@@ -31,6 +46,16 @@ describe "UserPages" do
 
         it "should create a user" do
           expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+          before {click_button submit}
+          let(:user) {User.find_by(email: "user@example.com")}
+
+          it {should have_title(user.name)}
+          it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+          it {should have_selector('img', minimum: 1)}
+          it {should have_selector('h1', text: user.name)}
         end
       end
     end
